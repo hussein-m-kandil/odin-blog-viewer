37e99a1d9a08474b34f3c40154758e586e0f1ffd
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-09-15 10:57:02 +0300

    Add helper function for setting URL params

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function setURlParams(
+  url: string | URL,
+  params: Record<string, string>
+) {
+  const srcUrl = url instanceof URL ? url : new URL(url);
+  const paramsEntries = Object.entries(params);
+  for (const [k, v] of paramsEntries) {
+    srcUrl.searchParams.set(k, v);
+  }
+  return srcUrl.href;
 }

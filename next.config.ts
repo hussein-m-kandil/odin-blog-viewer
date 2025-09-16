1e76f724bbcc142062f4d898268a84280f297e5a
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2025-09-16 16:51:28 +0300

    Add list of image qualities to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
+    qualities: [75, 85, 95, 100],
     remotePatterns: [
       {
         hostname: 'ndauvqaezozccgtddhkr.supabase.co',

ff45bf12b9db935b3d22e7062dd09a30daf26914
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2026-02-02 13:15:00 +0200

    Allow images from any origin
    Allow displaying images from any origin to facilitate changing
    the storage provider on the backend.

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     qualities: [75, 85, 95, 100],
-    remotePatterns: [
-      {
-        hostname: 'ndauvqaezozccgtddhkr.supabase.co',
-        pathname: '/storage/**',
-        protocol: 'https',
-      },
-    ],
+    remotePatterns: [{ hostname: '*' }],
   },
 };
 
